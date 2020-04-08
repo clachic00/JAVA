d0f589e0708ab9791aa23956c7a5a6b763ea53eb
clachic <anajrwk2@gmail.com>
2020-04-08 18:23:33 +0900

    1

---
+++ java_project/FirstJAVA/src/chapter1/Firstjava.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+public class Firstjava {
+
+	public static void main(String[] args) {
+		
+		for (int x = 1; x<10; x+=1) {
+		for (int y = 2; y<10; y+=1) {
+		
+		System.out.print(y+"x"+x+"="+x*y+"\t");
+		
+		
+		
+		} System.out.println();
+		}
+	}
+
+}
---
+++ java_project/FirstJAVA/src/chapter1/reviewTest.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class reviewTest {
+
+	public static void main(String[] args) {
+	
+	
+	
+	}
+}
