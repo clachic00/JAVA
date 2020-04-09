d4d6b4cb6a830ce0693fbed80ff37e3918804dd6
clachic <anajrwk2@gmail.com>
2020-04-09 09:34:58 +0900

    1

--- java_project/FirstJAVA/src/chapter1/reviewTest.java
+++ java_project/FirstJAVA/src/chapter1/reviewTest.java
@@ -5,6 +5,8 @@ public class reviewTest {
 	public static void main(String[] args) {
 	
 	
+		System.out.println( "2+5=" + 2+5 );
+		System.out.println( "2+5=" + (2+5) );
 	
 	}
 }
---
+++ java_project/FirstJAVA/src/chapter1/reviewTest2.java
@@ -0,0 +1,28 @@
+package chapter1;
+
+public class reviewTest2 {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(15);
+		System.out.println(3*5);
+		System.out.println("15");
+		
+		for (int a=0; a<16; a+=1 ) {
+			if (a==15) {
+				System.out.println(a);
+			}
+			
+			
+		}
+		int c = 5;
+		int d = 3;
+		System.out.println(c*d);
+		
+		}
+		
+		
+	}
+
+
