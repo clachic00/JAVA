f2eb8d3b7e9e2a146ce4e61d690d8fcd3ec3a79e
clachic00 <anajrwk2@gmail.com>
2020-04-24 04:17:58 +0900

    Update ChangeArray.java

--- java_project/Cp8-Array/src/arrayPractice/ChangeArray.java
+++ java_project/Cp8-Array/src/arrayPractice/ChangeArray.java
@@ -2,42 +2,35 @@ package arrayPractice;
 
 public class ChangeArray {
 
+	int[][] array(int arr[][]) {
 
-	ChangeArray array(int[][] arr){
-		
-		for (i <  )
-		
-		
-		
-		return ;
+		int[] ext = arr[arr.length-1];
+		for (int i = arr.length-1; i > 0; i--) {
+
+			
+			arr[i] = arr[i - 1];
+			
+			
+		}
+		arr[0] = ext;
+		return arr;
 	}
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public static void main(String[] args) {
-		
-		int arr[][] = new int[][] {
-			{1,2,3},
-			{4,5,6},
-			{7,8,9}
-		};
-		
+
+		int arr[][] = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 },{10,11,12} };
+
 		ChangeArray ca = new ChangeArray();
-		
+
 		ca.array(arr);
+
 		
-		
-		
-		
+		for(int[] e: arr) {
+		for(int f: e) {
+			System.out.print(f+" ");
+		}
+		System.out.println();
+		}
 		
 		
 	}
