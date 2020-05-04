7eeece09f9822c6cbcaa47445ee70a5c44da9d6a
clachic <anajrwk2@gmail.com>
2020-05-04 17:00:05 +0900

    middle

---
+++ java_project/Cp14-Collection/src/ex/HashSetTest.java
@@ -0,0 +1,32 @@
+package ex;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetTest {
+
+	public static void main(String[] args)
+	{
+	//HashSet<String> hSet=new HashSet<String>();
+	HashSet<String> hSet = new HashSet<>();
+	
+	hSet.add("First");
+	hSet.add("Second");
+	hSet.add("Third");
+	hSet.add("First");
+	System.out.println("저장된 데이터 수: "+hSet.size());
+	
+	Iterator<String> itr = hSet.iterator();
+	
+	while(itr.hasNext())
+		System.out.println(itr.next());
+	
+	
+	
+	
+	
+	
+	}
+
+
+}
