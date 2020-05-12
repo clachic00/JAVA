1a5585e95b6fb829edbe3b2c45ff656319596359
clachic <anajrwk2@gmail.com>
2020-05-12 18:20:05 +0900

    FINAL

---
+++ java_project/JjjRpg_Yang/src/개선할점.java
@@ -0,0 +1,25 @@
+
+public class 개선할점 {
+	
+	
+	void 1.변수선언() {
+		System.out.println(
+		 "많은 변수 선언 -> 소통은 x 정리x(혼잡)");
+	}
+	void 2.틀() {
+		System.out.println(
+		"추가하기전에 틀을 더 잘 잡았어야한다");
+	}
+	void 3.테스트() {
+		System.out.println(
+		"정보가 많아서 오류테스트를 할게 많은데 테스트가 부족한채로 커밋해서 혼란");
+	}
+	void 4.파악() {
+		System.out.println(
+		"욕심만 많아서 해놓은 것을 검토하지 않고 기능만 늘리려 했다");
+	}
+	void 5.주석() {
+		System.out.println(
+		"주석달기");
+	}
+}
