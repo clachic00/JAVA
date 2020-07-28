55d40049f8dc96895f635d8fc3cf03067b5a6ebf
clachic <anajrwk2@gmail.com>
2020-07-28 13:16:31 +0900

    first-draft

---
+++ jsp_project/Ditest/src/test/Service.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Service {
+
+	
+	
+	
+	MemberDao dao;
+	
+	public void setDao(MemberDao dao) {
+		this.dao=dao;
+	}
+	
+	
+	public void print() {
+		dao.print();
+	}
+	
+	
+}
